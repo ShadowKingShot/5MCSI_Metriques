417bb747a5ff95ecf30deb63c68973611e723fe1
ShadowKingShot <153279888+ShadowKingShot@users.noreply.github.com>
2024-11-20 17:30:52 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,38 +1,64 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, rendertemplatestring, render_template, jsonify
+from flask import render_template
+from flask import json
 from datetime import datetime
-import json
-from collections import Counter
+from urllib.request import urlopen
+import sqlite3
 
-app = Flask(__name__)
+app = Flask(__name)
 
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')
+    return render_template('hello.html') #Comm2
 
 @app.route("/contact/")
 def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
     return render_template('contact.html')
 
+
+@app.route('/tawarano/')
+def meteo():
+    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
+
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+
+@app.route("/histogramme/")
+def mongraphiquehistogramme():
+    return render_template("graphique histogramme.html")
+
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+    minutes = date_object.minute
+    return jsonify({'minutes': minutes})
 @app.route('/commits/')
-def display_commits():
-    # URL de l'API GitHub
+def displaycommits():
     url = "https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits"
     response = urlopen(url)
-    raw_content = response.read()
-    commits = json.loads(raw_content.decode('utf-8'))
+    rawcontent = response.read()
+    commits = json.loads(rawcontent.decode('utf-8'))
 
-    # Compter les commits par minute
-    commit_counts = [0] * 60
+    commitcounts = [0] * 60
     for commit in commits:
         commit_date = commit['commit']['author']['date']
         date_object = datetime.strptime(commit_date, '%Y-%m-%dT%H:%M:%SZ')
         commit_counts[date_object.minute] += 1
 
-    # Générer un tableau des données pour le HTML
     data = [{'minute': i, 'count': commit_counts[i]} for i in range(60)]
 
-    # Rendre le template HTML avec les données
     return render_template('commits.html', data=data)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name == "__main":
+  app.run(debug=True)
